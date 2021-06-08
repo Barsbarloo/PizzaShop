d72fe3b87621acc8f6ccfdf2dff2beeb7cef7f6a
Barsbarloo <lootanke@gmail.com>
2021-06-08 23:54:28 +0600

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -14,3 +14,18 @@ function add_to_cart(id)
     window.localStorage.setItem(key, x);
 
 } 
+
+function cart_get_number_of_items()
+{
+    var cnt = 0;
+    for(var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i); // getting key
+        var value = window.localStorage.getItem(key); // getting value
+        if (key.indexOf('product_') == 0)
+        { 
+            cnt = cnt + value*1;
+        }
+    }
+    return cnt;
+}
